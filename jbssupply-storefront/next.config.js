7d0d798b0f836c40ffe5b8616349df09c592ddc8
Jerry Cami Jean Baptiste <jerry.cami.jeanbaptiste@gmail.com>
2025-11-27 14:46:32 -0500

    +

--- jbssupply-storefront/next.config.js
+++ jbssupply-storefront/next.config.js
@@ -18,26 +18,31 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
-      },
-    ],
-  },
+images: {
+  remotePatterns: [
+    {
+      protocol: "http",
+      hostname: "localhost",
+    },
+    {
+      protocol: "https",
+      hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+    },
+    {
+      protocol: "https",
+      hostname: "medusa-server-testing.s3.amazonaws.com",
+    },
+    {
+      protocol: "https",
+      hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
+    },
+    {
+      protocol: "https",
+      hostname: "s3.us-east-1.amazonaws.com",
+    },
+  ],
+},
+
 }
 
 module.exports = nextConfig
