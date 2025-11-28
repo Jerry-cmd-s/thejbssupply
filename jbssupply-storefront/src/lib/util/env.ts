b8f1a1138b545525d3a7aafb367b1a11b7497df0
Jerry Cami Jean Baptiste <jerry.cami.jeanbaptiste@gmail.com>
2025-11-28 14:55:05 -0500

    +

--- jbssupply-storefront/src/lib/util/env.ts
+++ jbssupply-storefront/src/lib/util/env.ts
@@ -1,3 +1,7 @@
+//export const getBaseURL = () => {
+  //return process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
+//}
+
 export const getBaseURL = () => {
-  return process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
+  return "https://jbssupply.medusajs.app"
 }
